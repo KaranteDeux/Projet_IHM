0d543dd49a358329ae70b81ed5aa9a188923ead4
wassim24 <pixplosion24@gmail.com>
2016-03-29 23:18:06 +0200

    On a enfin les notes qui s'imbriquent. On a le menu latéral.

--- js/custom-interface.js
+++ js/custom-interface.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 
-    $(".button-collapse").sideNav({
-        menuWidth: 300, // Default is 240
-        edge: 'right', // Choose the horizontal origin
+    $(".open-sidenav").sideNav({
+        menuWidth: 240, // Default is 240
+        edge: 'left', // Choose the horizontal origin
         closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
+
+
     $('.collapsible').collapsible();
+
 });
