aa7ada38b1642eca2d8006bea12d0173ad9bf9de
wassim24 <pixplosion24@gmail.com>
2016-04-07 19:39:13 +0200

    Ajout de pleins pleins de trucs. Le développement de la barre de recherche est faite. L'organisation par modules est faite. L'ajout d'une note est faite. Le partage est fait. Il reste la page de profil. Les notes partagées. On peut maintenant afficher les notes. Le bouton rouge en bas comprends les mouvements, glisser la souris a gauche pour afficher les modules. Passer la souris vers le haut pour les autres options.

---
+++ js/notes-interface.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    var $beforeLeft = '0%';
+    var $beforeTop = "0%";
+
+    $('.card-content').on('click', function () {
+
+        var $note = $(this).parent().parent()[0];
+        var $overlay = $('.overlay-note')[0];
+
+        $(this).parent().parent().addClass('opened-note-item');
+
+        $beforeLeft = $note.style.getPropertyValue('left');
+        $beforeTop = $note.style.getPropertyValue('top');
+
+        $note.style.setProperty('position', 'absolute');
+        $note.style.setProperty('left', '24%');
+        $note.style.setProperty('top', '0');
+        $note.style.setProperty('z-index', '10');
+        $note.style.setProperty('width', '60%');
+        $note.style.setProperty('transition', 'all .25s');
+        $note.style.setProperty('transition-timing-function', 'ease-in-out');
+        $overlay.style.setProperty('display', 'block');
+    });
+
+    $('.overlay-note').on('click', function () {
+
+        var $note = $('.opened-note-item')[0];
+        $note.style.setProperty('left', $beforeLeft);
+        $note.style.setProperty('top', $beforeTop);
+        $note.style.removeProperty('width');
+        $note.style.removeProperty('z-index');
+
+        this.style.setProperty('display', 'none');
+
+        $('.opened-note-item').removeClass("opened-note-item");
+    });
+
+
+    var $grid = $('.row').isotope({
+        itemSelector: '.note-item',
+    });
+
+    $('.IHM').on('click', function(){
+        $grid.isotope({filter : '.ihm'});
+    });
+
+    $('.SVL').on('click', function(){
+        $grid.isotope({filter : '.svl'});
+    });
+
+    $('.M3DS').on('click', function(){
+        $grid.isotope({filter : '.m3ds'});
+    });
+
+    $('.CAR').on('click', function(){
+        $grid.isotope({filter : '.car'});
+    });
+
+    $('.ALL').on('click', function(){
+        $grid.isotope({filter : '.note-item'});
+    });
+})
